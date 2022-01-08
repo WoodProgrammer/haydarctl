3f1e1dc06afb17dc29d90f9631de79d0c459234a
WoodProgrammer <asddeneme3@gmail.com>
2022-01-08 14:41:49 +0300

    terragrunt state handler updated

---
+++ src/terragrunt.py
@@ -0,0 +1,15 @@
+import json
+import subprocess
+
+class Terragrunt(object):
+    def __init__(self):
+        pass
+
+    def fetch_list_of_state_files(self): ## function to fetch state files from remote address
+        pass
+
+    def state_checker(self): ## that is responsible to fetch states from the remote address
+        pass
+
+    def aggregator(self): ## aggregate plan output with issue templates
+        pass

4dd2c79470258bd4bc91b58d5715adf69a366de8
WoodProgrammer <asddeneme3@gmail.com>
2022-01-16 02:26:09 +0300

    code blocks adjusted according to the usage

--- src/issue_generator.py
+++ src/issue_generator.py
@@ -1,17 +1,17 @@
 from jinja2 import Environment, FileSystemLoader
 
+
 class TerragruntIssueGenerator(object):
     def __init__(self):
-        env = Environment(loader=FileSystemLoader('templates'), autoescape=True)
+        env = Environment(
+            loader=FileSystemLoader('templates'), 
+            autoescape=True)
 
         self.template = env.get_template('terragrunt_gh_issue_templates/issue_template.jinja')
-    
 
     def create_template_file(self, repo, plan_output, module_name):
-
         output_from_parsed_template = self.template.render(plan_output="{}".format(plan_output), module_name=module_name)
         return output_from_parsed_template
-
 
     def save_template_content(self, repo, template_directory, content):
 
